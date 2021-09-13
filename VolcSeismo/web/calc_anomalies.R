2ae870f7b477f7152706aed709e674068d3fa139
Israel Brewster <ijbrewster@alaska.edu>
2021-09-13 14:16:45 -0800

    Replace R script with new one that produces two images - short and long term

--- VolcSeismo/web/calc_anomalies.R
+++ VolcSeismo/web/calc_anomalies.R
@@ -27,59 +27,80 @@ ORDER BY datetime"
 
     #data=data.frame(read.csv(file = "/Users/israel/Downloads/AUCH-BHZ-2021-06-29T00_00_00Z-2021-09-06T23_59_59Z.csv"))
     print("Beginning calc anomalies function")
-    date_plot = c();corcoef1_low = c();corcoef1_up = c();corcoef1_estimate = c()
-    date = chron(substr(as.character(data$date),1,10),format=c('y-m-d'))
-    days = unique(date)
+    date_plot=c();
+    corcoef1_low_longterm=c();corcoef1_up_longterm=c();corcoef1_estimate_longterm=c()
+    corcoef1_low_shortterm=c();corcoef1_up_shortterm=c();corcoef1_estimate_shortterm=c()
+    date=chron(substr(as.character(data$date),1,10),format=c('y-m-d'))
+    days=unique(date)
 
-    for (i in seq(10,length(days), by = 1)){
-        if (length(which(date==days[i]))>3600){
-            training=data[which(date<days[i]),]
-            test=data[which(date==days[i]),]
-
-            a1 = hist(training$freq_max10, breaks = seq(0,20, by=0.1), plot='FALSE')
-            a2 = hist(test$freq_max10, breaks = seq(0,20, by=0.1), plot = 'FALSE')
+    for(i in seq(10,length(days), by=1)){
+    if(length(which(date==days[i]))>3600){
         
-            test=cor.test(a1$density,a2$density,method = c("pearson"))
-            corcoef1_low[i]=abs(test$conf.int[1])
-            corcoef1_up[i]=abs(test$conf.int[2])
-            corcoef1_estimate[i]=test$estimate
-            date_plot[i]=days[i]
-        }
-    }
-    dp_dates <- as.Date(date_plot)
+        training_longterm=data[which(date<days[i]),]
+        training_shortterm=data[which(date>=(days[i]-3) & date<days[i]),]
+        test=data[which(date==days[i]),]
+
+        a1_longterm=hist(training_longterm$freq_max10[which(training_longterm$freq_max10>=0.5)], breaks = seq(0.5,20, by=0.1), plot='FALSE')
+        a1_shortterm=hist(training_shortterm$freq_max10[which(training_shortterm$freq_max10>=0.5)], breaks = seq(0.5,20, by=0.1), plot='FALSE')     
+        a2=hist(test$freq_max10[which(test$freq_max10>=0.5)], breaks = seq(0.5,20, by=0.1), plot='FALSE')     
+
+        test_longterm=cor.test(a1_longterm$density,a2$density,method = c("pearson"))
+        corcoef1_low_longterm[i]=abs(test_longterm$conf.int[1])
+        corcoef1_up_longterm[i]=abs(test_longterm$conf.int[2])
+        corcoef1_estimate_longterm[i]=test_longterm$estimate
     
+        test_shortterm=cor.test(a1_shortterm$density,a2$density,method = c("pearson"))
+        corcoef1_low_shortterm[i]=abs(test_shortterm$conf.int[1])
+        corcoef1_up_shortterm[i]=abs(test_shortterm$conf.int[2])
+        corcoef1_estimate_shortterm[i]=test_shortterm$estimate        
+        
+        date_plot[i]=days[i]
+    }
+    }
     filename <- paste(
         destdir,
         '/',
         station,
-        ".png",
+        "-1.png",
         sep = ""
     )
-    print(filename)
-    
+
     png(file=filename, width=7.5, height = 4, units="in", res=600)
-    plot(dp_dates,
-         100 * (1 - corcoef1_estimate),
-         type = "l",
-         ylim = c(0, 100),
-         xlim = c(dp_dates[10],
-         dp_dates[length(date_plot)] + 7),
-         ylab = "Anomaly level (%)",
-         xlab = "")
-    polygon(c(dp_dates, rev(dp_dates)),
-            c(100 * (1 - corcoef1_low), rev(100 * (1 - corcoef1_up))),
-            col = "#87CEEB", border = "black")
 
-    set.seed(1)
-    fit <- nnetar(100 * (1 - corcoef1_estimate), lambda = 0.7)
-    fcast <- forecast(fit, PI = TRUE, h = 7)
-    x_vector = (dp_dates[length(date_plot)] + 1):(dp_dates[length(date_plot)] + 7)
+    dp_asdate=as.Date(date_plot)
 
-    polygon(c(x_vector, rev(x_vector)),
-            c(fcast$lower[1:7,1], rev(fcast$upper[1:7, 1])),
-            col = rgb(1, 0, 0, 0.3), border = "black")
-    abline(h = 50, col = "gray", lty = 2)
+    par(mar=c(2,4.1,.5,2.1))
+
+    plot(dp_asdate,100*(1-corcoef1_estimate_longterm), 
+         type = "l",ylim = c(0,100),
+         xlim=c(dp_asdate[10], dp_asdate[length(date_plot)]), 
+         ylab = "Long-term Anomaly (%)",xlab="")
+
+    polygon(c(dp_asdate, rev(dp_asdate)), 
+            c(100*(1-corcoef1_low_longterm), rev(100*(1-corcoef1_up_longterm))),
+            col = "#87CEEB", border='black')
+    abline(h=50, col="gray", lty=2)
+
+    filename <- paste(
+        destdir,
+        '/',
+        station,
+        "-2.png",
+        sep = ""
+    )
+
+    png(file=filename, width=7.5, height = 4, units="in", res=600) 
+    par(mar=c(2,4.1,.5,2.1))
     
+    plot(dp_asdate,100*(1-corcoef1_estimate_shortterm), 
+         type = "l", ylim = c(0,100),
+         xlim=c(dp_asdate[10], dp_asdate[length(date_plot)]), 
+         ylab = "Short-term Anomaly (%)", xlab="")
+
+    polygon(c(dp_asdate, rev(dp_asdate)), 
+            c(100*(1-corcoef1_low_shortterm), rev(100*(1-corcoef1_up_shortterm))),
+            col = "#87CEEB", border='black')
+    abline(h=50, col="gray", lty=2)
     dev.off()
 }
 
