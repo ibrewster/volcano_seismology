ae46216bb7985df362ea39cabdf3ce188637704b
Israel Brewster <ijbrewster@alaska.edu>
2021-09-14 13:52:35 -0800

    Activate virtualenv before switching directories to avoid potential issues with a . path

--- generate_anomalies.sh
+++ generate_anomalies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd "${0%/*}/VolcSeismo/web"
 source "${0%/*}/bin/activate"
+cd "${0%/*}/VolcSeismo/web"
 python calc_anomalies.py
 rsync --delete --recursive /apps/volcano_seismology/VolcSeismo/web/static/img/anomalies apps.avo.alaska.edu:/volcano_seismology/VolcSeismo/web/static/img/
 
