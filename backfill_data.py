31f9910bb300d0ab3b676a4932a9857b0c6997d8
root <root@novarupta.snap.uaf.edu>
2022-05-31 22:42:40 +0000

    Drop processor count on anomalies script to reduce resource usage

--- backfill_data.py
+++ backfill_data.py
@@ -4,7 +4,7 @@ from VolcSeismo.config import config
 from obspy import UTCDateTime
 
 if __name__ == "__main__":
-    START = UTCDateTime("2021-12-24T16:00:09+00")
+    START = UTCDateTime("2021-12-30T13:40:00.000000+00")
     STOP = UTCDateTime("2022-01-03T19:13:30+00")
     interval = config['GLOBAL'].getint('minutesperimage', 10)
     while START <= STOP:
